342d836aa36b9ce037a118b06120dd508661711c
Kenvins <863178540@qq.com>
2021-07-16 22:14:14 +0800

    init

---
+++ src/main.h
@@ -0,0 +1,8 @@
+#ifndef DIFFIE_HELLMAN_MAIN_H
+#define DIFFIE_HELLMAN_MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <openssl/aes.h>
+#include <openssl/md5.h>
+#endif
